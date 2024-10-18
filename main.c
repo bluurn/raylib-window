b386a68d20c8abc9b78558fb01619c4294addbdb
vladimir.suvorov <vladimir.suvorov@xempus.com>
2024-10-18 16:39:03 +0200

    Toggle fullscreen and show hint

--- main.c
+++ main.c
@@ -2,39 +2,55 @@
 #define WINDOW_TITLE "This is my first Raylib app"
 #define INIT_SCREEN_WIDTH  1024
 #define INIT_SCREEN_HEIGHT 768
-#define FONT_SIZE 60
+#define BASE_FONT_SIZE 60
 
 // TODO: Focus window on start
 // TODO: Draw some shapes
 // TODO: Try animations
 
-
-void DrawTextCenter(const char* text, int fontSize, Color color)
+Vector2 GetTextUpperLeftCorner(const char * text, int fontSize)
 {
-  const int screenWidth = GetScreenWidth();
-  const int screenHeight = GetScreenHeight();
-  const int textOffsetWidth = MeasureText(text, fontSize);
-  const int posX = (screenWidth / 2) - (textOffsetWidth / 2);
-  const int posY = screenHeight / 2 - fontSize;
-  DrawText(text, posX, posY, fontSize, color);
+  const float screenWidth = GetScreenWidth();
+  const float screenHeight = GetScreenHeight();
+  const float textOffsetWidth = MeasureText(text, fontSize);
+
+  Vector2 result = {
+    .x = (screenWidth / 2) - (textOffsetWidth / 2),
+    .y = screenHeight / 2 - fontSize
+  };
+
+  return result;
+}
+
+void DrawHint()
+{
+  const char * text  = "Press [F] to toggle Fullscreen";
+  const int fontSize = BASE_FONT_SIZE / 2;
+  Vector2 ltCorner = GetTextUpperLeftCorner(text, fontSize);
+  DrawText(text, ltCorner.x, ltCorner.y + fontSize, fontSize, LIGHTGRAY);
+}
+
+void DrawHero(void)
+{
+  const int fontSize = BASE_FONT_SIZE;
+  const char * text  = "Hello, Raylib <3";
+  Vector2 ltCorner = GetTextUpperLeftCorner(text, fontSize);
+  DrawText(text, ltCorner.x, ltCorner.y, fontSize, RAYWHITE);
 }
 
 int main(void)
 {
-  SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_TOPMOST);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_TOPMOST | FLAG_WINDOW_ALWAYS_RUN);
   SetTargetFPS(60);
   InitWindow(INIT_SCREEN_WIDTH, INIT_SCREEN_HEIGHT, WINDOW_TITLE);
 
-  const char * text  = "Hello, AWESOME Raylib <3";
-
   while(!WindowShouldClose()) {
     if(IsKeyDown(KEY_F)) !IsWindowMaximized() ? MaximizeWindow() : RestoreWindow();
-      
+  
     BeginDrawing();
-
     ClearBackground(DARKGRAY);
-    DrawTextCenter(text, FONT_SIZE, RAYWHITE);
-
+    DrawHero();
+    DrawHint();
     EndDrawing();
   }
 
