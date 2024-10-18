064e1f45dff095b3e522ee510347da4c6e1758fc
vladimir.suvorov <vladimir.suvorov@xempus.com>
2024-10-18 11:24:22 +0200

    first working raylib app! YAY

--- main.c
+++ main.c
@@ -1,9 +1,33 @@
-#include <stdio.h>
-
 #include "raylib.h"
+#include <stdio.h>
+#include <string.h>
 
-int main(int argc, char ** argv)
+int main(void)
 {
-  printf("Hello, world2!\n");
+  const int screenWidth = 1024;
+  const int screenHeight = 768;
+  const int fontSize = 40;
+  const char * text = "Hello, Raylib <3";
+  const char * title = "This is my first Raylib app";
+
+
+  SetTargetFPS(60);
+  InitWindow(screenWidth, screenHeight, title);
+
+  const int textOffsetWidth = MeasureText(text, fontSize);
+
+  while(!WindowShouldClose()) {
+
+    BeginDrawing();
+
+    ClearBackground(DARKGRAY);
+
+    DrawText(text, (screenWidth / 2) - (textOffsetWidth / 2), screenHeight / 2 - fontSize, fontSize, RAYWHITE);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
   return 0;
 }
