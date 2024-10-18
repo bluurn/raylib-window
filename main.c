55bac588056f2b20fb059b36d20e5d328ade4aa7
vladimir.suvorov <vladimir.suvorov@xempus.com>
2024-10-18 15:45:24 +0200

    Automatically center text

--- main.c
+++ main.c
@@ -1,33 +1,42 @@
 #include "raylib.h"
+#define INIT_SCREEN_WIDTH  1024
+#define INIT_SCREEN_HEIGHT 768
+#define FONT_SIZE 60
 
-void DrawTextCenter(const char* text, int screenWidth, int screenHeight, int fontSize, Color color)
+
+void DrawTextCenter(const char* text, int fontSize, Color color)
 {
+  const int screenWidth = GetScreenWidth();
+  const int screenHeight = GetScreenHeight();
   const int textOffsetWidth = MeasureText(text, fontSize);
   const int posX = (screenWidth / 2) - (textOffsetWidth / 2);
   const int posY = screenHeight / 2 - fontSize;
   DrawText(text, posX, posY, fontSize, color);
 }
 
+
+// TODO: Press F to fullscreen
 int main(void)
 {
-  const int screenWidth = 1024;
-  const int screenHeight = 768;
-  const int fontSize = 40;
-  const char * text = "Hello, Raylib <3";
+  int screenWidth    = INIT_SCREEN_WIDTH;
+  int screenHeight   = INIT_SCREEN_HEIGHT;
+  const int fontSize = FONT_SIZE;
   const char * title = "This is my first Raylib app";
+  const char * text  = "Hello, AWESOME Raylib <3";
 
-
-  SetConfigFlags(FLAG_WINDOW_TOPMOST);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   SetTargetFPS(60);
   InitWindow(screenWidth, screenHeight, title);
 
   while(!WindowShouldClose()) {
+    screenWidth  = GetScreenWidth();
+    screenHeight = GetRenderHeight();
 
     BeginDrawing();
 
     ClearBackground(DARKGRAY);
 
-    DrawTextCenter(text, screenWidth, screenHeight, fontSize, RAYWHITE);
+    DrawTextCenter(text, fontSize, RAYWHITE);
 
     EndDrawing();
   }
